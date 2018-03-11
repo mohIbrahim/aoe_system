4712725ea5023a47e6200fb6022ed69f79fb5b26
Mohammed Ibrahim <mohibrahimqop@gmail.com>
2018-03-11 09:37:52 +0200

    adding john User

--- database/seeds/DeveloperUserSeeder.php
+++ database/seeds/DeveloperUserSeeder.php
@@ -19,13 +19,21 @@ class DeveloperUserSeeder extends Seeder
     	$moh->name = "Mohammed Ibrahim Fawzy";
     	$moh->email = "mohibrahimqop@gmail.com";
     	$moh->password = bcrypt("123456789");
-    	$moh->save();
+		$moh->save();
+		
+		$joh = new User();
+		$joh->name = 'John Sameh';
+		$joh->email = 'j.sameh@infomed-me.com';
+		$joh->password = bcrypt('J12341234');
+		$joh->save();
 
     	$role = new Role();
     	$role->name = "Developer";
     	$role->save();
 
-    	$moh->roles()->attach($role);
+		$moh->roles()->attach($role);
+		$joh->roles()->attach($role);
+		
     	$permissions = Permission::all();
 
     	foreach ($permissions as $permission) {
