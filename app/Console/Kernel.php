f03b8c8e5e13b9d9a63f8be3c0d47aa38da4bf64
Mohammed Ibrahim <mohibrahimqop@gmail.com>
2018-05-05 16:27:58 +0200

    finish database backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('backup:database')->everyMinute();
+        $schedule->command('backup:database')->dailyAt('13:00');;
     }
 
     /**
