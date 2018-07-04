0e3956604fc4794dcd6c17ddd30042a062e1986f
Mohammed Ibrahim <mohibrahimqop@gmail.com>
2018-07-04 23:00:26 +0200

    daily commit

---
+++ resources/views/dashboard/maintenance_engineers/blocks/printing_machines.blade.php
@@ -0,0 +1,45 @@
+<table class="table table-striped table-hover table-bordered">
+    <thead>
+        <h4 class="text-center"> آلات الطباعة المعينة إليكَ </h3>
+        <tr>
+            <th>رقم الإشارة</th>
+            <th>نوع الإشارة</th>
+            <th>حالة الإشارة</th>
+            <th>اسم العميل</th>
+            <th>المنطقة</th>
+            <th>تاريخ الإشارة</th>
+            <th>كود الآلة</th>
+            <th>إنشاء زيارة</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($lastAssignedReferences as $referenceKey => $reference)
+            <tr>
+                <td>
+                    <a href="{{action('ReferenceController@show', ['id'=>$reference->id])}}">
+                        {{ $reference->code }}
+                    </a>
+                </td>
+                <td>{{ $reference->type }}</td>
+                <td>{{ $reference->status }}</td>
+                <td>
+                    <a href="{{action('CustomerController@show', ['id'=>(($reference->printingMachine)?(($reference->printingMachine->customer)?($reference->printingMachine->customer->id):('')):(''))])}}">
+                        {{ ($reference->printingMachine)?(($reference->printingMachine->customer)?($reference->printingMachine->customer->name):('')):('') }}
+                    </a>
+                </td>
+                <td>{{ ($reference->printingMachine)?(($reference->printingMachine->customer)?($reference->printingMachine->customer->area):('')):('') }}</td>
+                <td>{{ $reference->received_date }}</td>
+                <td>
+                    <a href="{{ action('PrintingMachineController@show', ['id'=>(($reference->printingMachine)?(($reference->printingMachine->id)?($reference->printingMachine->id):('')):(''))]) }}">
+                        {{ ($reference->printingMachine)?(($reference->printingMachine->code)?($reference->printingMachine->code):('')):('') }}
+                    </a>
+                </td>
+                <td>
+                    <a href="{{ action('VisitController@createWithPrintingMachineId', ['pm_id'=>(($reference->printingMachine)?(($reference->printingMachine->id)?($reference->printingMachine->id):('')):(''))]) }}">
+                        إنشاء زيارة جديدة
+                    </a>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
